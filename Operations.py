c333c17d3dcb2106173334bbb3c64d235d14d669
YANQUB <165962820+YANQUB@users.noreply.github.com>
2024-04-04 13:44:50 +0100

    Add files via upload

---
+++ Operations.py
@@ -0,0 +1,12 @@
+print(" TESTS DES 4 OPERATIONS SUIVANTES")
+
+
+somme = 20 + 2
+difference = 50 - 30
+produit = 50 * 6
+quotient = 10 / 2
+
+print("Somme de 20 et 2 donne :", somme)
+print("Difference 50 et 30 donne:", difference)
+print("Produit de 50 et 6 donne :", produit)
+print("Quotient  de 10 par 2 donne :", quotient)
